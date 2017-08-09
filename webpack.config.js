cf45cd10f93e6762581de9a29a9c58b4f1494c06
Christian Freitas <chrfreitas@gmail.com>
2017-08-08 21:01:36 -0300

    Added dependencies and wepack file

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+let config = {
+    devtool: 'source-map',
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js',
+        sourceMapFilename: '[file].map'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+}
+
+module.exports = config;
