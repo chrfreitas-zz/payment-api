c0a765e54d5a7a4029dd82745d9b2611897a4f38
cfreitas <chrfreitas@gmail.com>
2017-08-10 12:36:47 -0300

    Implemented a example code

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,36 @@
 
-window.buy = (a) => {
-    console.log(a);
-}
+'use strict';
 
-console.log(PaymentRequest);
+window.buy = () => {
+
+    if (!window.PaymentRequest) {
+        throw "Your browser doesn't suppport PaymentRequest";
+    }
+
+    let methodData = [
+        {
+            supportedMethods: ['visa', 'mastercard']
+        }
+    ];
+
+    let details = {
+        total: {
+            label: 'Something that costs money',
+            amount: {
+                currency: 'GBP',
+                value: '9.99'
+            }
+        }
+    };
+
+
+    new PaymentRequest(methodData, details)
+      .show()
+      .then(function(uiResult) {
+        //sucess
+      })
+      .catch(function(error) {
+        //error
+      });
+
+}
