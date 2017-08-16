29d5e6806e983cb3aa79ee8dd15a2e368e65a299
Christian Freitas <chrfreitas@gmail.com>
2017-08-15 21:02:26 -0300

    Removed webpack wasn't necessary to solve the problem

--- src/js/app.js
+++ src/js/app.js
@@ -1,59 +1,57 @@
+(() => {
 
-'use strict';
+    'use strict';
 
+    window.buy = () => {
 
-window.buy = () => {
-
-    if (!window.PaymentRequest) {
-        throw "Your browser doesn't suppport PaymentRequest";
-    }
-
-    let methodData = [{
-            supportedMethods: ['visa', 'mastercard'],
-            data: {
-                supportedTypes: ['credit', 'debit']
-            }
+        if (!window.PaymentRequest) {
+            throw "Your browser doesn't suppport PaymentRequest";
         }
-    ];
 
+        let methodData = [{
+                supportedMethods: ['visa', 'mastercard'],
+                data: {
+                    supportedTypes: ['credit', 'debit']
+                }
+            }
+        ];
 
-    let products = [
-        {
-            label: 'House',
-            amount: {
-                currency: 'USD',
-                value: '10.00'
+        let products = [
+            {
+                label: 'House',
+                amount: {
+                    currency: 'USD',
+                    value: '10.00'
+                }
+            },
+            {
+                label: 'Car',
+                amount: {
+                    currency: 'USD',
+                    value: '24.99'
+                }
             }
-        },
-        {
-            label: 'Car',
-            amount: {
-                currency: 'USD',
-                value: '24.99'
-            }
+        ];
+
+        let details = {
+            total: {
+                label: 'Total',
+                amount: {
+                    currency: 'USD',
+                    value: 34.99
+                }
+            },
+            displayItems: products
         }
-    ];
 
+        new PaymentRequest(methodData, details)
+            .show()
+            .then(function(uiResult) {
+                alert('Success');
+            })
+            .catch(function(error) {
+                alert('Error');
+            });
 
-    let details = {
-        total: {
-            label: 'Total',
-            amount: {
-                currency: 'USD',
-                value: 34.99
-            }
-        },
-        displayItems: products
     }
-
-
-    new PaymentRequest(methodData, details)
-        .show()
-        .then(function(uiResult) {
-            alert('Success');
-        })
-        .catch(function(error) {
-            alert('Error');
-        });
-
-}
+})();
